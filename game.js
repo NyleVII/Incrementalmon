1f1f9f763d61cdba85cab9361a15ceb76c3c2661
NyleVII <stephenantymis@gmail.com>
2017-01-16 22:20:04 -0700

    Added author, version, and cleaned up code

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
+//AUTHOR: Stephen Antymis
+//VERSION: 0.0.1 Jan 16, 2017
 //set width and height variables for game
 var width = 800;  //480
-var height = 600; //320 TEST EDIT
+var height = 600; //320
 //create game object and initialize the canvas
 var game = new Phaser.Game(width, height, Phaser.AUTO, null, {preload: preload, create: create, update: update});
 
